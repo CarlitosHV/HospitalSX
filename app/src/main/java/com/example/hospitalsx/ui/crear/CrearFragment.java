3fe4a62024c4fd32c1481fffb36e4e464be6cef5
CarlitosHV <carloshvsa21@hotmail.com>
2022-09-06 09:43:38 -0500

    ultimos cambios añadidos

--- app/src/main/java/com/example/hospitalsx/ui/crear/CrearFragment.java
+++ app/src/main/java/com/example/hospitalsx/ui/crear/CrearFragment.java
@@ -2,6 +2,8 @@ package com.example.hospitalsx.ui.crear;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.DatePickerDialog;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +13,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Button;
 
 import com.example.hospitalsx.R;
 
+import java.util.Calendar;
+
 public class CrearFragment extends Fragment {
+
+    private Button btnLimpiar, btnGuardar;
+    private EditText etID, etNombre, etFecha, etEdad, etEstatura, etPeso;
+    private Spinner spnArea, spnDr, spnGenero;
+    private ImageView ivFoto;
+    private ImageButton btnCalendario;
+
+    DatePickerDialog dpd;
+    Calendar cal;
+
+    private static int anio, mes, dia;
+    public static String img = "", a, d, sex;
+    public static final int REQUEST_TAKE_PHOTO =1;
+    private Uri photoUri;
 
     private CrearViewModel mViewModel;
 
@@ -25,7 +48,37 @@ public class CrearFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_crear, container, false);
+        View root = inflater.inflate(R.layout.fragment_crear, container, false);
+
+        Componentes(root);
+
+        return root;
+    }
+
+    private void Componentes (View root){
+        EditTextComponentes(root);
+        ButtonComponentes(root);
+        SpinnerComponentes(root);
+    }
+
+    private void EditTextComponentes(View root){
+        etID = root.findViewById(R.id.etCID);
+        etNombre = root.findViewById(R.id.etCnombre);
+        etFecha = root.findViewById(R.id.etCFechaIngresoP);
+        etEdad = root.findViewById(R.id.etCEdad);
+        etEstatura = root.findViewById(R.id.etCEstatura);
+        etPeso = root.findViewById(R.id.etCPeso);
+    }
+
+    private void ButtonComponentes(View root){
+        btnLimpiar = root.findViewById(R.id.btnClimpiar);
+        btnGuardar = root.findViewById(R.id.btnCguardar);
+        ivFoto = root.findViewById(R.id.ivcFoto);
+        btnCalendario = root.findViewById(R.id.ibtnCFechaIngreso);
+    }
+
+    private void SpinnerComponentes(View root){
+
     }
 
     @Override
