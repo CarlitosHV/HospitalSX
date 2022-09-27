2f7ef53298e51ce7a98837a8c8e2bff6b8431adb
CarlitosHV <carloshvsa21@hotmail.com>
2022-09-27 11:17:07 -0500

    Versión funcional del 27 de sep

--- app/src/main/java/com/example/hospitalsx/ui/crear/CrearFragment.java
+++ app/src/main/java/com/example/hospitalsx/ui/crear/CrearFragment.java
@@ -21,12 +21,14 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.hospitalsx.R;
 import com.example.hospitalsx.bd.sqlite;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class CrearFragment extends Fragment implements  View.OnClickListener, AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
 
@@ -103,9 +105,9 @@ public class CrearFragment extends Fragment implements  View.OnClickListener, Ad
         spnGenero = root.findViewById(R.id.spnCgenero);
         spnGenero.setAdapter(generoAdapter);
 
-        spnArea.setOnClickListener(this);
-        spnDr.setOnClickListener(this);
-        spnGenero.setOnClickListener(this);
+        spnArea.setOnItemSelectedListener(this);
+        spnDr.setOnItemSelectedListener(this);
+        spnGenero.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -115,19 +117,51 @@ public class CrearFragment extends Fragment implements  View.OnClickListener, Ad
         // TODO: Use the ViewModel
     }
 
-    @Override
-    public void onClick(View view) {
 
-    }
 
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
         switch (adapterView.getId()){
             case R.id.spnCareaP:
+                ArrayAdapter<CharSequence> areaAdapter;
+
+                switch (position){
+                    case 1:
+                        areaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.o1, android.R.layout.simple_spinner_item);
+                        break;
+                    case 2:
+                        areaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.o2, android.R.layout.simple_spinner_item);
+                        break;
+                    case 3:
+                        areaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.o3, android.R.layout.simple_spinner_item);
+                        break;
+                    case 4:
+                        areaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.o4, android.R.layout.simple_spinner_item);
+                        break;
+                    default:
+                        areaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.opciones, android.R.layout.simple_spinner_item);
+                        break;
+                }
+                if(position!=0){
+                    a = adapterView.getItemAtPosition(position).toString();
+                }else{
+                    a="";
+                }
+                spnDr.setAdapter(areaAdapter);
                 break;
             case R.id.spnCdoctor:
+                if(position!=0){
+                    d = adapterView.getItemAtPosition(position).toString();
+                }else{
+                    d="";
+                }
                 break;
             case R.id.spnCgenero:
+                if(position!=0){
+                    sex = adapterView.getItemAtPosition(position).toString();
+                }else{
+                    sex="";
+                }
                 break;
         }
     }
@@ -140,5 +174,71 @@ public class CrearFragment extends Fragment implements  View.OnClickListener, Ad
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
         etFecha.setText(day + "/" + (month+1) + "/" + year);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()) {
+            case R.id.ibtnCFechaIngreso:
+                cal = Calendar.getInstance();
+                anio = cal.get(Calendar.YEAR);
+                mes = cal.get(Calendar.MONTH);
+                dia = cal.get(Calendar.DAY_OF_MONTH);
+                dpd = new DatePickerDialog(getContext(), this, anio, mes, dia);
+                dpd.show();
+                break;
+            case R.id.btnClimpiar:
+                limpiar();
+                break;
+                //Hacer funcionar esta parte
+            case R.id.ivcFoto:
+                break;
+            case R.id.btnCguardar:
+                if (etID.getText().equals("") || etNombre.getText().equals("")
+                || etPeso.getText().equals("") || etEstatura.getText().equals("") || etEdad.getText().equals("") || etFecha.getText().equals("")
+                ){
+                    Toast.makeText(getContext(), "Campos vacíos, verifique la información", Toast.LENGTH_SHORT).show();
+                }else{
+                    int id = Integer.parseInt(etID.getText().toString());
+                    a = a.toUpperCase();
+                    d = d.toUpperCase();
+                    String nom = etNombre.getText().toString().toUpperCase();
+                    sex = sex.toUpperCase();
+                    String fecha = etFecha.getText().toString().trim();
+                    String edad = etEdad.getText().toString().trim();
+                    String estatura = etEstatura.getText().toString().trim();
+                    String peso = etPeso.getText().toString().trim();
+                    sqlite.abrir();
+                    if (sqlite.addRegistroPaciente(id, a, d, nom, sex, fecha, edad, estatura, peso, img)){
+                        Toast.makeText(getContext(), "Información guardada con éxito", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getContext(), "Error al guardar la información", Toast.LENGTH_SHORT).show();
+                    }
+                    sqlite.cerrar();
+                }
+                break;
+        }
+    }
+
+    private void limpiar(){
+        etID.setText("");
+        etNombre.setText("");
+        etFecha.setText("");
+        etEdad.setText("");
+        etEstatura.setText("");
+        etPeso.setText("");
+        ivFoto.setImageResource(R.drawable.ic_menu_camera);
+        a="";
+        d="";
+        sex="";
+
+        ArrayAdapter<CharSequence> areaAdapter, drAdapter, generoAdapter;
+        areaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.opciones, android.R.layout.simple_spinner_item);
+        drAdapter = ArrayAdapter.createFromResource(getContext(), R.array.o0, android.R.layout.simple_spinner_item);
+        generoAdapter = ArrayAdapter.createFromResource(getContext(), R.array.sx, android.R.layout.simple_spinner_item);
+        spnArea.setAdapter(areaAdapter);
+        spnDr.setAdapter(drAdapter);
+        spnGenero.setAdapter(generoAdapter);
+
     }
 }
--- app/src/main/java/com/example/hospitalsx/ui/listar/ListarFragment.java
+++ app/src/main/java/com/example/hospitalsx/ui/listar/ListarFragment.java
@@ -1,22 +1,41 @@
 package com.example.hospitalsx.ui.listar;
 
+import androidx.core.content.FileProvider;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hospitalsx.R;
+import com.example.hospitalsx.bd.sqlite;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class ListarFragment extends Fragment {
 
     private ListarViewModel mViewModel;
+    ArrayList<String> registros;
+    ArrayList<String> imagenes;
+
+    sqlite sqLite;
 
     public static ListarFragment newInstance() {
         return new ListarFragment();
@@ -25,7 +44,52 @@ public class ListarFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_listar, container, false);
+        View root = inflater.inflate(R.layout.fragment_listar, container, false);
+
+        ListView list = root.findViewById(R.id.lvlListaPacientes);
+        sqLite = new sqlite(getContext());
+
+        sqLite.abrir();
+        Cursor cursor = sqLite.getRegistro();
+        registros = sqLite.getPacientes(cursor);
+        imagenes = sqLite.getImagenes(cursor);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_list_item_1, registros);
+
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialogo_paciente, null);
+                ((TextView) dialogView.findViewById(R.id.tvdpInfoPaciente)).setText(registros.get(i));
+
+                ImageView ivImagen = dialogView.findViewById(R.id.ivDPFoto);
+                cargarImagen(imagenes.get(i), ivImagen);
+                AlertDialog.Builder dialogo = new AlertDialog.Builder(getContext());
+                dialogo.setTitle("Paciente");
+                dialogo.setView(dialogView);
+                dialogo.setPositiveButton("Aceptar", null);
+                dialogo.show();
+            }
+        });
+
+
+        sqLite.cerrar();
+
+        return root;
+    }
+
+    public void cargarImagen(String imagen, ImageView iv){
+        try {
+            File file = new File(imagen);
+            Uri uriPhoto = FileProvider.getUriForFile(getContext(), "com.example.HospitalSX", file);
+            iv.setImageURI(uriPhoto);
+        }catch(Exception ex){
+            Toast.makeText(getContext(), "Error al cargar la imagen", Toast.LENGTH_SHORT).show();
+            Log.d("Carga de imagen", "Error al cargar imagen" + imagen + "\n Mensaje" + ex.getMessage() +
+                    "\n Causa" + ex.getCause());
+        }
     }
 
     @Override
